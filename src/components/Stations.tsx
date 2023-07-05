6bf7cfb6b4bde3e541271addc472fe1b61d7d7f0
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-07-05 16:34:46 +0100

    API works

--- src/components/Stations.tsx
+++ src/components/Stations.tsx
@@ -1,55 +1,129 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { fetchStations } from '../helpers/ApiCallHelper';
+import Select from 'react-select';
+import { fetchStations, getJourneyInfo } from '../helpers/ApiCallHelper';
+import { apiError, JourneyType, OptionsType, StationType } from '../types/stationType';
 
 const Stations: React.FC = () => {
-    const [allStations, setAllStations] = useState<string[]>([]);
-    const [selectedStations, setSelectedStations] = useState<{ fromStation: string; toStation: string }>({ fromStation: '', toStation: '' });
+    const [allStations, setAllStations] = useState<StationType[]>([]);
+    const [fromStation, setFromStation] = useState<string>('');
+    const [toStation, setToStation] = useState<string>('');
+    const [journeyInfo, setJourneyInfo] = useState<JourneyType[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<apiError>();
 
-    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectedStations({ ...selectedStations, [event.target.name]: event.target.value });
-
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        console.log(toStation);
+        //window.location.href = `https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/${selectedStations.fromStation}/${selectedStations.toStation}/#LiveDepResults`;
+        await getJourneyInfo(fromStation, toStation)
+            .then((value) => {
+                console.log(value);
+                setJourneyInfo(value.outboundJourneys);
+            })
+            .catch((err) => {
+                console.log(err);
+                setError(err);
+            })
+            .finally(() => setLoading(true));
     };
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        window.location.href = `https://www.lner.co.uk/travel-information/travelling-now/live-train-times/depart/${selectedStations.fromStation}/${selectedStations.toStation}/#LiveDepResults`;
-    };
+    const options: OptionsType[] = [];
+    allStations.map(station => (
+        options.push({ value: station.crsCode, label: station.stationName })
+    ));
 
     useEffect(() => {
-        // fetchStations()
-        //     .then((value) => console.log(value))
-        //     .catch((err) => console.log(err))
-        //     .finally(() => console.log('finally'));
-        setAllStations(['NLT', 'WRU', 'WCX', 'MYB', 'HWY', 'GER']);
+        setLoading(false);
+        fetchStations()
+            .then((value) => console.log(value))
+            .catch((err) => console.log(err))
+            .finally(() => console.log('finally'));
+        setAllStations([
+            {
+                stationName: 'Northolt Park',
+                crsCode: 'NLT',
+            },
+            {
+                stationName: 'West Ruislip',
+                crsCode: 'WRU',
+            },
+            {
+                stationName: 'Wembley Central',
+                crsCode: 'WCX',
+            },
+            {
+                stationName: 'Marylebone',
+                crsCode: 'MYB',
+            },
+            {
+                stationName: 'High Wycombe',
+                crsCode: 'HWY',
+            },
+            {
+                stationName: 'Gerrards Cross',
+                crsCode: 'GER',
+
+            },
+        ]);
     }, []);
 
     return (
         <div className = "stations-container">
 
             <h1 id = "Welcome">Stations!</h1>
-            <div >
+            <div>
                 <form onSubmit = { handleSubmit } className = "stations-form">
                     <div>
                         <label htmlFor = "fromStation">From:</label>
-                        <select key = { 'fromStation' } id = { 'fromStation' } name = { 'fromStation' } onChange = { handleChange }  >
-                            {allStations.map(station => (
-                                <option key = { station } value = { station }>{station}</option>
-                            ))},
-                        </select>
+                        <Select
+                            onChange = { (options: OptionsType | null) => {
+                                if(options) {
+                                    setFromStation(options.value);
+                                }} }
+                            options = { options }
+                        />
                     </div>
                     <div>
                         <label htmlFor = "toStation">To:</label>
-                        <select id = { 'toStation' } name = { 'toStation' } onChange = { handleChange }  >
-                            {allStations.map(station => (
-                                <option key = { station } value = { station }>{station}</option>
-                            ))},
-                        </select>
+                        <Select
+                            onChange = { (options: OptionsType | null) => {
+                                if(options) {
+                                    setToStation(options.value);
+                                }
+                            } }
+                            options = { options }
+                        />
                     </div>
 
                     <button type = "submit">Submit</button>
-
                 </form>
+            </div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Departure Platform</th>
+                            <th>Departure Time</th>
+                            <th>Arrival Time</th>
+                            <th>Duration</th>
+                            <th>Changes</th>
+                            <th>Operator</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {loading && journeyInfo.map((journey) => (
+                            <tr key = { journey.departureTime }>
+                                <td> 7 </td>
+                                <td>{new Date(journey.departureTime).toLocaleTimeString()}</td>
+                                <td>{new Date(journey.arrivalTime).toLocaleTimeString()}</td>
+                                <td>{journey.journeyDurationInMinutes}</td>
+                                <td>{journey.legs.length - 1}</td>
+                                <td>{journey.primaryTrainOperator.name}</td>
+
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
--- src/helpers/ApiCallHelper.tsx
+++ src/helpers/ApiCallHelper.tsx
@@ -3,5 +3,29 @@ export const fetchStations = () => {
         headers: {
             'X-API-KEY': `${process.env.REACT_APP_X_API_KEY}`,
         },
-    });
+    }).then((response) => response.json());
 };
+
+export const getJourneyInfo = (originStation: string, destinationStation: string) => {
+    return fetch('https://mobile-api-softwire2.lner.co.uk/v1/fares?'+ new URLSearchParams({
+        originStation: originStation,
+        destinationStation: destinationStation,
+        noChanges: 'false',
+        numberOfAdults: '2',
+        numberOfChildren: '0',
+        journeyType: 'single',
+        outboundDateTime: '2023-07-06',
+        outboundIsArriveBy: 'false',
+    }), {
+        headers: {
+            'X-API-KEY': `${process.env.REACT_APP_X_API_KEY}`,
+        },
+    }).then((response) => {
+        if(response.status!==200) {throw response.json();}
+        return response.json();
+    }).catch((error) => {
+        console.log('');
+    });
+
+};
+
